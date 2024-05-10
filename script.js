894971b3d31fae19f2300648917dbe4efcd9fed0
DomeManca <83721834+DomeManca@users.noreply.github.com>
2024-05-10 10:43:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,32 +91,3 @@ let rate = NORMAL_PLAYBACK_RATE;
 
 const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
 if (mediaQuery.matches) rate = REDUCED_PLAYBACK_RATE;
-
-const words = [
-  "godo",
-  "troia",
-  "giroud",
-  "frocio",
-  "negro",
-  "tonyboy",
-  "giocanethug",
-];
-
-textReplace(words, "target-word", rate);
-
-function textReplace(words, targetElement, rate) {
-  let wordIndex = 0;
-
-  const randomWordElement = document.getElementById(targetElement);
-
-  const changeWordWithAnimation = () => {
-    randomWordElement.style.opacity = 0; // Fade out
-    setTimeout(function () {
-      wordIndex = (wordIndex + 1) % words.length;
-      randomWordElement.textContent = words[wordIndex];
-      randomWordElement.style.opacity = 1; // Fade in
-    }, 50);
-  };
-
-  const interval = setInterval(changeWordWithAnimation, rate);
-}
